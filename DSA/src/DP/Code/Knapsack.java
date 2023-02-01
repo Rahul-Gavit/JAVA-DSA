6a66d80e7457c0248860fb68d8bb6bcd75183a8d
Rahul-519 <rgavit519@gmail.com>
2023-02-02 00:09:08 +0530

    DP

--- DSA/src/DP/Code/Knapsack.java
+++ DSA/src/DP/Code/Knapsack.java
@@ -89,6 +89,37 @@ public class Knapsack {
         print(dp);
         return dp[n][W];
     }
+
+    public static int unboundedKnapsackT(int val[], int wt[], int W){
+        int n = val.length;
+        int dp[][] = new int[n+1][W+1];
+        
+        for(int i=0; i<dp.length; i++){ //0th col
+            dp[i][0] = 0;
+        }
+        for(int j=0; j<dp[0].length; j++){ // 0th row
+            dp[0][j] = 0;
+        }
+
+        for(int i=1; i<n+1; i++){
+            for(int j=1; j<W+1; j++){
+                int v = val[i-1]; //ith item val
+                int w = wt[i-1]; // ith item wt
+
+                if(w <= j){ // valid
+                    int incProfit = v + dp[i][j-w];
+                    int excProfit = dp[i-1][j];
+                    dp[i][j] = Math.max(incProfit, excProfit);
+
+                }else{ // invalid
+                    int excProfit = dp[i-1][j];
+                    dp[i][j] = excProfit;
+                }
+            }
+        }
+        print(dp);
+        return dp[n][W];
+    }
     public static void main(String[] args) {
         int val[] = {15, 14, 10, 45, 30};
         int wt[] = {2, 5, 1, 3, 4};
@@ -104,5 +135,6 @@ public class Knapsack {
         System.out.println(knapsack(val, wt, W, val.length));
         System.out.println(knapsackM(val, wt, W, val.length, dp));
         System.out.println(knapsackT(val, wt, W));
+        System.out.println(unboundedKnapsackT(val, wt, W));
     }
 }
