5aa51ebc413fe6127e3a953485641251f1ef194b
Rahul-519 <rgavit519@gmail.com>
2023-02-05 14:57:33 +0530

    DP-final

--- DSA/src/DP/Code/Mcm.java
+++ DSA/src/DP/Code/Mcm.java
@@ -41,6 +41,43 @@ public class Mcm {
         }
         return dp[i][j] = minCost;
     }
+
+    //Tabulation Approach
+    public static int mcmT(int arr[]){
+        int n = arr.length;
+        int dp[][] = new int[n][n];
+
+        for(int i=0; i<n; i++){
+            dp[i][i] = 0;
+        }
+
+        //bottom-Up
+        for(int len=2; len<=n-1; len++){
+            for(int i=1; i<=n-len; i++){
+                int j = i+len-1; //col
+                dp[i][j] = Integer.MAX_VALUE;
+                for(int k=i; k<=j-1; k++){
+                    int cost1 = dp[i][k];
+                    int cost2 = dp[k+1][j];
+                    int cost3 = arr[i-1] * arr[k] * arr[j];
+                    dp[i][j] = Math.min(dp[i][j], cost1+cost2+cost3);
+                }
+            }
+        }
+        print(dp);
+        return dp[1][n-1];
+    }
+
+    public static void print(int dp[][]){
+
+        for(int i=0; i<dp.length; i++){
+            for(int j=0; j<dp[0].length; j++){
+                System.out.print(dp[i][j]+" ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
     public static void main(String[] args) {
         int arr[] = {1, 2, 3, 4, 3};
         int n = arr.length;
@@ -50,5 +87,6 @@ public class Mcm {
             Arrays.fill(dp[i], -1);
         }
         System.out.println(mcmM(arr, 1, n-1, dp));
+        System.out.println(mcmT(arr));
     }
 }
