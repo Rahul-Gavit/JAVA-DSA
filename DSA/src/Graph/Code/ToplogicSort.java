ca54e78516e8bb5fad2268dab20bdce15357e027
Rahul-519 <rgavit519@gmail.com>
2023-02-14 13:19:20 +0530

    Graphs

--- DSA/src/Graph/Code/ToplogicSort.java
+++ DSA/src/Graph/Code/ToplogicSort.java
@@ -20,6 +20,8 @@ public class ToplogicSort {
             graph[i] = new ArrayList<>();
         }
 
+        graph[0].add(new Edge(0, 3));
+
         graph[2].add(new Edge(2, 3));
 
         graph[3].add(new Edge(3, 1));
@@ -33,6 +35,7 @@ public class ToplogicSort {
     }
 
     //O(V+E)
+    //Topological sort using DFS
     public static void topSort(ArrayList<Edge>[] graph){
         boolean vis[] = new boolean[graph.length];
         Stack<Integer> s = new Stack<>();
@@ -60,10 +63,65 @@ public class ToplogicSort {
 
         s.push(curr);
     }
+
+    //Topological Sort Using BFS
+
+    public static void calcIndeg(ArrayList<Edge>[] graph, int indeg[]){
+        for(int i=0; i<graph.length; i++){
+            int v = i;
+            for(int j=0; j<graph[v].size(); j++){
+                Edge e = graph[v].get(j);
+                indeg[e.dest]++;
+            }
+        }
+    }
+
+    public static void topSort1(ArrayList<Edge> graph[]){
+        int indeg[] = new int[graph.length];
+        calcIndeg(graph, indeg);
+        Queue<Integer> q = new LinkedList<>();
+
+        for(int i=0; i<graph.length; i++){
+            if(indeg[i] == 0){
+                q.add(i);
+            }
+        }
+
+        //BFS
+        while(!q.isEmpty()){
+            int curr = q.remove();
+            System.out.print(curr+" ");
+
+            for(int i=0; i<graph[curr].size(); i++){
+                Edge e = graph[curr].get(i);
+                indeg[e.dest]--;
+                if(indeg[e.dest] == 0){
+                    q.add(e.dest);
+                }
+            }
+        }
+        System.out.println();
+    }
+
+    //print all paths
+    public static void printAllPath(ArrayList<Edge> graph[], int src, int dest, String path){ // TC -> exponential
+        if(src == dest){
+            System.out.println(path+dest);
+            return;
+        }
+
+        for(int i=0; i<graph[src].size(); i++){
+            Edge e = graph[src].get(i);
+            printAllPath(graph, e.dest, dest, path+src);
+        }
+    }
     public static void main(String[] args) {
         int V = 6;
         ArrayList<Edge> graph[] = new ArrayList[V];
         createGraph(graph);
         topSort(graph);
+        topSort1(graph);
+        int src = 5, dest = 1;
+        printAllPath(graph, src, dest, "");
     }
 }
